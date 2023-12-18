a5464ff28ecd72282c60c1e92396fd274f805ae8
Yianni1980 <ioagian1980@gmail.com>
2023-12-18 13:39:27 +0200

    Initial Commit

---
+++ LottieDemo/LottieDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  LottieDemoApp.swift
+//  LottieDemo
+//
+//  Created by ioannis on 18/12/23.
+//
+
+import SwiftUI
+
+@main
+struct LottieDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
